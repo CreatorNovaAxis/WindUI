a61f30397494b3ad952e510b1b105ddea1a3228d
CreatorNovaAxis <analyzpodr@gmail.com>
2026-05-09 19:16:15 +0500

    Update VStack.lua

--- src/elements/VStack.lua
+++ src/elements/VStack.lua
@@ -4,82 +4,166 @@ local New = Creator.New
 local Element = {}
 
 function Element:New(Config)
+
     local VStackModule = {
         __type = "VStack",
+
         Elements = {},
+
         ElementFrame = nil,
+        Layout = nil,
     }
-    
+
     local VStackFrame = New("Frame", {
-        Size = UDim2.new(1,0,0,0),
+        Name = "VStack",
+
+        Size = UDim2.new(1, 0, 0, 0),
+
         BackgroundTransparency = 1,
+
         AutomaticSize = "Y",
+
         Parent = Config.Parent,
-    }, {
-        New("UIListLayout", {
-            FillDirection = "Vertical",
-            HorizontalAlignment = "Center",
-            --VerticalAlignment = "Center",
-            Padding = UDim.new(0, Config.Tab and Config.Tab.Gap or (Config.Window.NewElements and 1 or 6))
-        }),
+    })
+
+    local Gap =
+        Config.Tab
+        and Config.Tab.Gap
+        or (Config.Window.NewElements and 1 or 6)
+
+    local Layout = New("UIListLayout", {
+        FillDirection = "Vertical",
+
+        HorizontalAlignment = "Center",
+
+        SortOrder = Enum.SortOrder.LayoutOrder,
+
+        Padding = UDim.new(0, Gap),
+
+        Parent = VStackFrame,
     })
 
     VStackModule.ElementFrame = VStackFrame
-    
+    VStackModule.Layout = Layout
+
     local ElementsModule = Config.ElementsModule
+
     ElementsModule.Load(
-        VStackModule, 
-        VStackFrame, 
+        VStackModule,
+        VStackFrame,
         ElementsModule.Elements,
-        Config.Window, 
+
+        Config.Window,
         Config.WindUI,
-        -- function(CurrentElement, AllElements)
-        --     local Gap = Config.Tab and Config.Tab.Gap or (Config.Window.NewElements and 1 or 6)
-            
-        --     local StretchableElements = {}
-        --     local TotalFixedWidth = 0
-            
-        --     for _, Element in next, AllElements do
-        --         if Element.__type == "Space" then
-        --             TotalFixedWidth = TotalFixedWidth + (Element.ElementFrame.Size.X.Offset or 6)
-        --         elseif Element.__type == "Divider" then
-        --             TotalFixedWidth = TotalFixedWidth + (Element.ElementFrame.Size.X.Offset or 1)
-        --         else
-        --             table.insert(StretchableElements, Element)
-        --         end
-        --     end
-            
-        --     local StretchCount = #StretchableElements
-        --     if StretchCount == 0 then return end
-            
-        --     local ElementWidthScale = 1 / StretchCount
-            
-        --     local TotalGapWidth = Gap * (StretchCount - 1)
-            
-        --     local TotalOffset = -(TotalGapWidth + TotalFixedWidth)
-            
-        --     local BaseOffset = math.floor(TotalOffset / StretchCount)
-        --     local Remainder = TotalOffset - (BaseOffset * StretchCount)
-            
-        --     for i, Element in next, StretchableElements do
-        --         local Offset = BaseOffset
-        --         if i <= math.abs(Remainder) then
-        --             Offset = Offset - 1
-        --         end
-                
-        --         if Element.ElementFrame then
-        --             Element.ElementFrame.Size = UDim2.new(ElementWidthScale, Offset, 1, 0)
-        --         end
-        --     end
-        -- end,  
+
         nil,
-        ElementsModule, 
-        Config.UIScale, 
+
+        ElementsModule,
+        Config.UIScale,
         Config.Tab
     )
+
+    function VStackModule:AddElement(element)
+        table.insert(self.Elements, element)
+    end
+
+    function VStackModule:RemoveElement(element)
+        local index = table.find(self.Elements, element)
+
+        if index then
+            table.remove(self.Elements, index)
+
+            pcall(function()
+                if element.Destroy then
+                    element:Destroy()
+
+                elseif element.ElementFrame then
+                    element.ElementFrame:Destroy()
+                end
+            end)
+        end
+    end
+
+    function VStackModule:Clear()
+
+        for _, element in ipairs(self.Elements) do
+
+            pcall(function()
+
+                if typeof(element) == "Instance" then
+                    element:Destroy()
+
+                elseif type(element) == "table" then
+
+                    if element.Destroy then
+                        element:Destroy()
+
+                    elseif element.ElementFrame then
+                        element.ElementFrame:Destroy()
+
+                    elseif element.UIElements
+                        and element.UIElements.Main then
+
+                        element.UIElements.Main:Destroy()
+                    end
+                end
+            end)
+        end
+
+        table.clear(self.Elements)
+    end
+
+    function VStackModule:Destroy()
+
+        self:Clear()
+
+        pcall(function()
+            if self.Layout then
+                self.Layout:Destroy()
+            end
+        end)
+
+        pcall(function()
+            if self.ElementFrame then
+                self.ElementFrame:Destroy()
+            end
+        end)
+
+        self.Layout = nil
+        self.ElementFrame = nil
+
+        table.clear(self.Elements)
+    end
+
+    function VStackModule:GetHeight()
+
+        if not self.ElementFrame then
+            return 0
+        end
+
+        return self.ElementFrame.AbsoluteSize.Y
+    end
     
+    function VStackModule:SetGap(value)
+
+        if self.Layout then
+            self.Layout.Padding = UDim.new(0, value)
+        end
+    end
+
+    function VStackModule:FindElementByType(typeName)
+
+        for _, element in ipairs(self.Elements) do
+            if element.__type == typeName then
+                return element
+            end
+        end
+    end
     
-    
+    function VStackModule:GetElements()
+        return self.Elements
+    end
+
     return VStackModule.__type, VStackModule
 end
 
